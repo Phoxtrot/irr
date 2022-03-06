fba060572565f6b2290fa2495ab6745ddad99bb8
Phoxtrot <yinkarhy@gmail.com>
2022-03-06 19:45:33 +0100

    fixed database

--- config/database.php
+++ config/database.php
@@ -1,5 +1,6 @@
+$DATABASE_URL=parse_url(postgres://vjqhoupsxutbyp:e13b2714ab327e8d5642622b4fe2d51d1d41a24680c97ba14aaddd207f76fc43@ec2-3-227-195-74.compute-1.amazonaws.com:5432/d6d9hf2vj01i6
+);
 <?php
-
 use Illuminate\Support\Str;
 
 return [
@@ -15,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +67,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['path'],"/"),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
